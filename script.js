c1621d1a5da1571ec66cce8d74b7adca52dbaa89
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-22 17:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
+function getFormvalue(event) {
+	event.preventDefault();
     //Write your code here
-
+	const fname=document.getElementsByName("fname")[0].value;
+	const lname=document.getElementsByName("lname")[0].value;
+	alert(fname+" "+lname);
 }
